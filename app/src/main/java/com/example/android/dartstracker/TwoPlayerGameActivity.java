dbb78e3c3bade8eb1267fa776d93d66ca3891cdc
JP <johnmcgrady@hotmail.co.uk>
2019-01-10 17:37:44 +0900

    Add onClickListener to enter button in Two Player activity and record the user input score

--- app/src/main/java/com/example/android/dartstracker/TwoPlayerGameActivity.java
+++ app/src/main/java/com/example/android/dartstracker/TwoPlayerGameActivity.java
@@ -2,6 +2,9 @@ package com.example.android.dartstracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.android.dartstracker.data.GameDbHelper;
@@ -18,5 +21,17 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.two_player_game);
+
+        // When the enter button is pressed, the score is recorded
+        Button enterScore = (Button) findViewById(R.id.enterScoreButton);
+        enterScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Read the score input by the user
+                mScoreEditText = (EditText) findViewById(R.id.singleScore);
+                String score = mScoreEditText.getText().toString();
+                Log.v("SCORE INPUT VALUE: ", score);
+            }
+        });
     }
 }

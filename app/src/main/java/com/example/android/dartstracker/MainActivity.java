fd5a0d5e8f7b65d448f867bbd3e7717275be22fb
JP <johnmcgrady@hotmail.co.uk>
2019-01-10 16:36:46 +0900

    Add class for two player game

--- app/src/main/java/com/example/android/dartstracker/MainActivity.java
+++ app/src/main/java/com/example/android/dartstracker/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.android.dartstracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.android.dartstracker.data.GameDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);

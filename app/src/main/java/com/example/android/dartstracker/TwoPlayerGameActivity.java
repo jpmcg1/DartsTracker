502788dcb4bf45a54f2a44160d4e032c7b4ddf8a
JP <johnmcgrady@hotmail.co.uk>
2019-01-11 16:24:35 +0900

    Create the database in TwoPlayerGameActivity and add 501 as the initial input into row 1 when the activity is created

--- app/src/main/java/com/example/android/dartstracker/TwoPlayerGameActivity.java
+++ app/src/main/java/com/example/android/dartstracker/TwoPlayerGameActivity.java
@@ -1,21 +1,34 @@
 package com.example.android.dartstracker;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.dartstracker.data.GameContract;
+import com.example.android.dartstracker.data.GameContract.GameEntry;
 import com.example.android.dartstracker.data.GameDbHelper;
+
+// TODO: After every successful click of the ENTER button, we need to find a way to shift between
+// player one and player 2 so that the scores are added to the correct columns in the database
 
 public class TwoPlayerGameActivity extends AppCompatActivity {
 
     // Edit text field for the score inputted by the user
     private EditText mScoreEditText;
-
+    // The score input by the user is save in this variable
     private int mIndividualScore;
+
+    // Current score of Player 1
+    private TextView playerOneCurrentScore;
+    // Current score of Player 2
+    private TextView playerTwoCurrentScore;
 
     // Database helper object
     private GameDbHelper mDbHelper;
@@ -24,6 +37,18 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.two_player_game);
+
+        // Create a database helper object for the game
+        mDbHelper = new GameDbHelper(getBaseContext());
+        // Create the actual database and make it writable in order to add to it
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Use a ContentValues object to insert the scores into the table
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(GameEntry.COLUMN_PLAYER_ONE, 501);
+        initialValues.put(GameEntry.COLUMN_PLAYER_TWO, 501);
+
+        long newRowId = database.insert(GameEntry.TABLE_NAME, null, initialValues);
 
         // When the enter button is pressed, the score is recorded
         Button enterScore = (Button) findViewById(R.id.enterScoreButton);
@@ -38,9 +63,13 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
                 if (isInteger(score)) {
                     mIndividualScore = Integer.parseInt(score);
                     Log.v("SCORE INPUT VALUE: ", Integer.toString(mIndividualScore));
+
+                    // Add the score into the database
+
+
                     // if the integer is not a number, a toast is shown to the user
                 } else {
-                    Toast.makeText(getBaseContext(), "PLEASE ENTER A VALID NUMBER",
+                    Toast.makeText(getBaseContext(), R.string.validScoreInput,
                             Toast.LENGTH_LONG).show();
                 }
             }

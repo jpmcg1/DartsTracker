ad82f6ee92fcefc708264a8ec012420ac7ddf8cf
JP <johnmcgrady@hotmail.co.uk>
2019-01-16 17:40:10 +0900

    Add a delete method to remove all data from the database - doesn't change the ID though it seems

--- app/src/main/java/com/example/android/dartstracker/TwoPlayerGameActivity.java
+++ app/src/main/java/com/example/android/dartstracker/TwoPlayerGameActivity.java
@@ -1,11 +1,9 @@
 package com.example.android.dartstracker;
 
 import android.content.ContentValues;
-import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,38 +12,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.android.dartstracker.data.GameContract;
 import com.example.android.dartstracker.data.GameContract.GameEntry;
 import com.example.android.dartstracker.data.GameDbHelper;
 
-// TODO: After every successful click of the ENTER button, we need to find a way to shift between
-// player one and player 2 so that the scores are added to the correct columns in the database
-
 public class TwoPlayerGameActivity extends AppCompatActivity {
 
+    // Current score of Player 1
+    private TextView playerOneCurrentScore;
+    // Current score of Player 2
+    private TextView playerTwoCurrentScore;
     // Edit text field for the score inputted by the user
     private EditText mScoreEditText;
+
+
     // The score input by Player 1 1 is saved in this variable
     private int mPlayerOneScore;
     // The score input by Player 2 is saved in this variable
     private int mPlayerTwoScore;
+    // Initial score
+    private int initialScore = 501;
 
     // Integers to track whose turn is next
     private int PLAYER_ONE_TURN = 1;
     private int PLAYER_TWO_TURN = 2;
     private int mCurrentTurn;
 
-    // Initial score
-    private int initialScore = 501;
 
     // ContentValues object to store the input for player 1 and 2 prior to insertion
     // into the database
     private ContentValues newInput = new ContentValues();
-
-    // Current score of Player 1
-    private TextView playerOneCurrentScore;
-    // Current score of Player 2
-    private TextView playerTwoCurrentScore;
 
     // Database helper object
     private GameDbHelper mDbHelper;
@@ -70,48 +65,70 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
         mPlayerTwoScore = initialScore;
 
         // Use a ContentValues object to insert the initial scores into the table
-        ContentValues initialValues = new ContentValues();
+        /*ContentValues initialValues = new ContentValues();
         initialValues.put(GameEntry.COLUMN_PLAYER_ONE, initialScore);
         initialValues.put(GameEntry.COLUMN_PLAYER_TWO, initialScore);
-        mDatabase.insert(GameEntry.TABLE_NAME, null, initialValues);
+        mDatabase.insert(GameEntry.TABLE_NAME, null, initialValues);*/
 
         // Set the initial scores in the app interface to 501 for each player
-        playerOneCurrentScore = (TextView) findViewById(R.id.playerOneScore);
+        playerOneCurrentScore = findViewById(R.id.playerOneScore);
         playerOneCurrentScore.setText(Integer.toString(initialScore));
-        playerTwoCurrentScore = (TextView) findViewById(R.id.playerTwoScore);
+        playerTwoCurrentScore = findViewById(R.id.playerTwoScore);
         playerTwoCurrentScore.setText(Integer.toString(initialScore));
 
+        // When you want to clear the table and delete all the entries
+        deleteAllData(GameEntry.TABLE_NAME);
+
         // When the enter button is pressed, the score is recorded
-        Button enterScore = (Button) findViewById(R.id.enterScoreButton);
+        Button enterScore = findViewById(R.id.enterScoreButton);
         enterScore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Read the score input by the user
-                mScoreEditText = (EditText) findViewById(R.id.singleScore);
+                mScoreEditText = findViewById(R.id.singleScore);
                 String score = mScoreEditText.getText().toString();
 
                 // Reset the score on the user interface to empty
                 mScoreEditText.setText("");
 
+                // Calculate the current score for each player
+                int currentTotalPlayerOne = currentScore(GameEntry.COLUMN_PLAYER_ONE, GameEntry.TABLE_NAME);
+                int currentTotalPlayerTwo = currentScore(GameEntry.COLUMN_PLAYER_TWO, GameEntry.TABLE_NAME);
+
+                // TODO: NEED TO CHANGE THESE IF/ELSE TO WHILE LOOPS SO THAT THE SCORE IS INSERTED BEFORE
+                // THE SCORES ARE INSERTED INTO THE DATABASE, NOT JUST A TOAST MESSAGE
+
+                // TODO: ALSO NEED TO DELETE ALL THE DATA IN THE SQL TABLE, OR ELSE THE CODE BELOW
+                // WILL INSTANTLY NOT WORK AS THE CURRENT TOTAL SCORES ARE NOT BETWEEN 0 AND 501
+
+                // TODO: NEED TO ADD SOMETHING THAT SAYS 'IS THE SCORE ZERO' AND IF YES,
+                // was the last shot a double,  and if yes to both
+                // GAME OVER AND SOMETHING HAPPENS... at the end of onCreate say 'if
+                // currentScore = 0 then blah blah? Or create new method?
+
                 // If it is Player 1's turn, add the score to the ContentValue for Player 1
                 if (mCurrentTurn == 1) {
-                    insertPlayerOneScore(score);
-
+                    if (isTheScoreValid(currentTotalPlayerOne, score)) {
+                        insertPlayerOneScore(score);
+                    } else {
+                        Toast.makeText(getBaseContext(), R.string.validScoreInput,
+                                Toast.LENGTH_LONG).show();
+                    }
                     // If it is Player 2's turn add the score to the ContentValue for Player 2
                 } else if (mCurrentTurn == 2) {
-                    insertPlayerTwoScore(score);
+                    if (isTheScoreValid(currentTotalPlayerTwo, score)) {
+                        insertPlayerTwoScore(score);
+                    } else {
+                        Toast.makeText(getBaseContext(), R.string.validScoreInput,
+                                Toast.LENGTH_LONG).show();
+                    }
 
                     // Add the data in the ContentValues object to the database
                     insertScoresIntoDatabase();
 
                     // Update the scores in the UI
-                    int currentTotalPlayerOne = currentScore(GameEntry.COLUMN_PLAYER_ONE, GameEntry.TABLE_NAME);
-                    Log.d("TOTAL SCORE: ", Integer.toString(currentTotalPlayerOne));
                     mPlayerOneScore = initialScore - currentTotalPlayerOne;
                     playerOneCurrentScore.setText(Integer.toString(mPlayerOneScore));
-
-                    int currentTotalPlayerTwo = currentScore(GameEntry.COLUMN_PLAYER_TWO, GameEntry.TABLE_NAME);
-                    Log.d("TOTAL SCORE: ", Integer.toString(currentTotalPlayerTwo));
                     mPlayerTwoScore = initialScore - currentTotalPlayerTwo;
                     playerTwoCurrentScore.setText(Integer.toString(mPlayerTwoScore));
                 }
@@ -119,7 +136,16 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
         });
     }
 
-    // Add Player 1 score to the ContentValue variable
+    // Is the score a valid one - in terms of is the new total score 1 or less
+    private boolean isTheScoreValid(int currentScore, String newScore) {
+        int newTotalScore = initialScore - currentScore - Integer.parseInt(newScore);
+        if (newTotalScore < 2) {
+            return false;
+        }
+        return true;
+    }
+
+    // Add Player 1 score to the ContentValues object
     private void insertPlayerOneScore(String score) {
         // Check to see if the input is a valid number between 0 and 180
         if (isInteger(score)) {
@@ -130,7 +156,7 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
             // Set the current turn to Player 2
             mCurrentTurn = PLAYER_TWO_TURN;
 
-            // if the integer is not a number, a toast is shown to the user
+            // if the integer is not a valid number, a toast is shown to the user
         } else {
             Toast.makeText(getBaseContext(), R.string.validScoreInput,
                     Toast.LENGTH_LONG).show();
@@ -142,14 +168,14 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
     private void insertPlayerTwoScore(String score) {
         // Check to see if the input is a valid number between 0 and 180
         if (isInteger(score)) {
-        // Add the score into the ContentValues object in preparation for addition
-        // into the database
-        newInput.put(GameEntry.COLUMN_PLAYER_TWO, Integer.parseInt(score));
+            // Add the score into the ContentValues object in preparation for addition
+            // into the database
+            newInput.put(GameEntry.COLUMN_PLAYER_TWO, Integer.parseInt(score));
 
-        // Set the current turn back to Player 1
-        mCurrentTurn = PLAYER_ONE_TURN;
+            // Set the current turn back to Player 1
+            mCurrentTurn = PLAYER_ONE_TURN;
 
-        // if the integer is not a number, a toast is shown to the user
+            // if the integer is not a valid number, a toast is shown to the user
         } else {
             Toast.makeText(getBaseContext(), R.string.validScoreInput,
                     Toast.LENGTH_LONG).show();
@@ -176,13 +202,18 @@ public class TwoPlayerGameActivity extends AppCompatActivity {
 
     // Method to get the current score
     private int currentScore(String player, String tableName) {
-            mDatabase = mDbHelper.getWritableDatabase();
-            // Access the player's column in the database and add up all the scores
-            try {
-                String sql = "SELECT TOTAL(" + player + ") FROM " + tableName;
-                return (int)DatabaseUtils.longForQuery(mDatabase, sql, null);
-            } finally {
+        mDatabase = mDbHelper.getWritableDatabase();
+        // Access the player's column in the database and add up all the scores
+        try {
+            String sql = "SELECT TOTAL(" + player + ") FROM " + tableName;
+            return (int) DatabaseUtils.longForQuery(mDatabase, sql, null);
+        } finally {
 
-            }
+        }
+    }
+
+    private void deleteAllData(String tableName) {
+        mDatabase = mDbHelper.getWritableDatabase();
+        mDatabase.execSQL("delete from " + tableName);
     }
 }
